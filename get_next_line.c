080f8e6a4170b16695f35c330bf615eb7fbf16a7
Miguel Paredes <mykjestic@gmail.com>
2023-05-28 15:42:11 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -5,52 +5,60 @@
 #define BUFFER_SIZE 42
 #endif
 
-char *str_append(char *line, char *buffer, size_t len) {
-	size_t i = 0;
-	size_t line_len = 0;
-	size_t j = 0;
-
-	if (line) {
-		while (line[line_len])
-			line_len++;
-	}
-	char *new_line = malloc((line_len + len + 1) * sizeof(char));
-	if (!new_line) return NULL;
-	while (line && line[i]) {
-		new_line[i] = line[i];
-		i++;
-	}
-	while (j < len) {
-		new_line[i + j] = buffer[j];
-		j++;
-	}
-	new_line[i + j] = '\0';
-	free(line);
-	return new_line;
+char *str_append(char *line, char *buffer, size_t len)
+{
+    size_t i = 0;
+    size_t line_len = 0;
+    if (line)
+	{
+        while (line[line_len])
+            line_len++;
+    }
+    char *new_line = malloc((line_len + len + 1) * sizeof(char));
+    if (!new_line) return NULL;
+    while (line && line[i])
+	{
+        new_line[i] = line[i];
+        i++;
+    }
+    size_t j = 0;
+    while (j < len)
+	{
+        new_line[i + j] = buffer[j];
+        j++;
+    }
+    new_line[i + j] = '\0';
+    free(line);
+    return new_line;
 }
 
-char *get_next_line(int fd) {
-	static char buffer[BUFFER_SIZE + 1];
-	char *line = NULL;
-	ssize_t read_bytes;
-	size_t i;
-	size_t j;
+char *get_next_line(int fd)
+{
+    static char buffer[BUFFER_SIZE + 1];
+    char *line = NULL;
+    ssize_t read_bytes;
+    size_t i, j;
 
-	while ((read_bytes = read(fd, buffer, BUFFER_SIZE)) >= 0) {
-		buffer[read_bytes] = '\0';
+    while ((read_bytes = read(fd, buffer, BUFFER_SIZE)) >= 0)
+	{
+        buffer[read_bytes] = '\0';
 
-		for (i = 0; i < read_bytes && buffer[i] != '\n'; i++);
+        for (i = 0; i < read_bytes && buffer[i] != '\n'; i++);
 
-		line = str_append(line, buffer, i);
-		if (!line) return NULL;
+        line = str_append(line, buffer, i);
+        if (!line) return NULL;
 
-		if (i < read_bytes && buffer[i] == '\n') {
-			for (j = i + 1; j <= read_bytes; j++)
-				buffer[j - i - 1] = buffer[j];
-			return line;
-		}
-		if (read_bytes < BUFFER_SIZE) return line;
-	}
-	free(line);
-	return NULL;
+        if (i < read_bytes && buffer[i] == '\n')
+		{
+            for (j = i + 1; j <= read_bytes; j++)
+                buffer[j - i - 1] = buffer[j];
+
+            for (; j <= BUFFER_SIZE; j++)
+                buffer[j - i - 1] = '\0';
+            return line;
+        }
+        if (read_bytes < BUFFER_SIZE) return line;
+    }
+    free(line);
+    return NULL;
 }
